e736fca66001e7d3d41dae0dab4d989232c6f0a0
Rob Hill <robert.hill0122@gmail.com>
2014-06-26 14:39:05 -0600

    First version of basic app.

--- app.rb
+++ app.rb
@@ -1,17 +1,30 @@
 require "sinatra/base"
-require "sinatra/reloader"
 
 class MyApp < Sinatra::Application
-  register Sinatra::Reloader
 
   def initialize
     super
-    @items = []
+    @items = ["item 1", "item 2", "item 3"]
   end
 
-  get "/" do
-    "items length: #{@items.length}"
+  get '/' do
+    erb :root
   end
+
+
+  get '/items' do
+    erb :items, :locals =>{:item_count => @items.length}
+  end
+
+  get '/items/new' do
+    erb :new
+  end
+
+  get '/items/:id' do
+    id = params[:id]
+    "This item's id is #{id.to_i + 1}"
+  end
+
 
   run! if app_file == $0
 end
